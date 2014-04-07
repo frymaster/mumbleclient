c75344c29eafc873b86c0844d37954ef935b7f4a
Philip Cass <frymaster@127001.org>
2014-04-07 21:53:56 +0100

    Added some high-level documentation to MumbleClient

--- mumbleclient/MumbleClient.py
+++ mumbleclient/MumbleClient.py
@@ -1,14 +1,15 @@
-import MumbleControlProtocol
-import MumbleVoiceProtocol
+import platform
+import time
+import collections
 
 from twisted.internet import reactor,defer, task
 from twisted.internet.protocol import Factory, Protocol
 from twisted.internet.endpoints import SSL4ClientEndpoint
 from twisted.internet.ssl import CertificateOptions
 
-import platform
-import time
-import collections
+import MumbleControlProtocol
+import MumbleVoiceProtocol
+
 
 class _ControlFactory(Factory):
 
@@ -18,23 +19,74 @@ class _ControlFactory(Factory):
     def buildProtocol(self,addr):
         return MumbleControlProtocol.MumbleControlProtocol(self.mumbleClient)
 
+
 class MumbleSettings(object):
+    """
+    Object to hold settings passed to a MumbleClient.
+
+    Settings used by the base client are:
+        defaults to "localhost".  At this time MumbleClient is ipv4 only
+        .port       defaults to 64738
+        .nickname   defaults to "MumblePythonBot"
+        .password   defaults to "None"
+        .SSLOptions By default a new instance of twisted.internet.ssl.CertificateOptions
+    You can assign to a custom instance to provide a client certificate
+                    and/or verify the server certificate. See the twisted documentation for details
+
+    You can pass in implementation-specific settings in this object.  They will be ignored by the base client.
+    """
+
     host="localhost"
     port=64738
     nickname="MumblePythonBot"
     SSLOptions=CertificateOptions()
     password=None
 
+
 class User(object):
+    """ Stores all information known about a user at this time"""
     pass
+
 
 class _MumbleState(object):
     numTCPPings=0
     avgTCPPing=0
     users=collections.defaultdict(User)
 
-class MumbleClient(object):
 
+class MumbleClient(object):
+    """
+    An object representing a mumble client which uses twisted as an event and network handler.
+        This should be inherited and methods overridden or implemented to create specific clients.
+
+    Client life-cycle:
+        - Client will connect to TCP control protocol on the specified host and port
+            .controlConnected is a twisted Deferred that will make a callback when this occurs
+        - After a sucessfull connection, client will try to send three control messages
+            versionMessage(), authenticationMessage() and codecVersionMessage() are called in order
+            and the results sent.  To alter the contents of these messages (assuming no setting exists),
+            it is probably easiest to override the function, call the parent to get the "base" message,
+            and alter what you wish
+        - The server should then send channel and user information
+        - The server will then send a ServerSync message. This triggers the .clientConnected callback,
+          and the ServerSyncReceived() method is called.
+        - Every 5 seconds, the pingMessage() method is called, and the message returned sent to the server
+        - When the client disconnects, the .clientDisconnected Deferred is triggered (probably via 
+            errback and not callback) and the connectionLost() method called.
+
+    In general, the client is informed of activity via method calls. Outside the object, the program is
+        informed of (some) activity via the 3 Deferred objects, with more details functionality being the
+        responsiblility of the implementer
+
+    In general, if a message Foo is received by the client, the method FooReceived(self,message) will be called.
+        See MumbleControlProtocol for a list of MessageTypes.  Some are implemented in this class and can be
+        overridden; some are not needed for base functionality but will be called if defined.  The exception is
+        the UDPTunnel message, which is one of two possible ways voice data can be received.  In these cases
+        the VoiceMessageRecieved() function is called whether the voice source was UDP or TCP.
+
+    (Note that UDP is not currently supported)
+
+    """
     sessionID=None
 
     def __init__(self,settings=None):
@@ -44,7 +96,7 @@ class MumbleClient(object):
         self.state=_MumbleState()
 
         self.point=SSL4ClientEndpoint(reactor, self.settings.host, self.settings.port,self.settings.SSLOptions)
-        deferred = self.point.connect(_ControlFactory(self))
+        self.controlConnected = self.point.connect(_ControlFactory(self))
         self.clientConnected = defer.Deferred()
         self.clientDisconnected = defer.Deferred()
 
@@ -82,7 +134,7 @@ class MumbleClient(object):
 
     def _TCPVoiceMessageReceived(self,data):
         prefix,session,data = MumbleVoiceProtocol.decodeAudioMessage(data)
-        self.TCPVoiceMessageReceived(prefix,session,data,TCP=True)
+        self.VoiceMessageReceived(prefix,session,data,TCP=True)
 
     def _unknownMessageReceived(self,type,data):
         pass
@@ -108,7 +160,7 @@ class MumbleClient(object):
     def connectionLost(self,reason):
         pass
 
-    def TCPVoiceMessageReceived(self,prefix,session,data,TCP=False):
+    def VoiceMessageReceived(self,prefix,session,data,TCP=False):
         pass
 
     def sendMessage(self,message):
@@ -160,6 +212,7 @@ class MumbleClient(object):
     def disconnect(self):
         self.controlProtocol.disconnect()
 
+
 class AutoChannelJoinClient(MumbleClient):
 
     def ChannelStateReceived(self,message):
@@ -172,6 +225,7 @@ class AutoChannelJoinClient(MumbleClient):
         newMessage.session = self.sessionID
         newMessage.channel_id=self.channelID
         self.sendMessage(newMessage)
+
 
 if __name__ == '__main__':
     c = MumbleClient()
